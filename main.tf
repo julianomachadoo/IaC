908e591aef4a4a92d353d29bd11bcd5bf2ebdbf8
Juliano <julmachadoramos@gmail.com>
2023-08-21 16:59:30 -0300

    ansible

--- main.tf
+++ main.tf
@@ -17,6 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name = "iac-aws-ubuntu"
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               cd /home/ubuntu
+  #               echo "<h1>Feito com Terraform</h1>" > index.html
+  #               nohup busybox httpd -f -p 8080 &
+  #               EOF
   tags = {
     Name = "InstanciaTF"
   }
